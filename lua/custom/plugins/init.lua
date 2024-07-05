7679187a5ffaa03a1e10534f851f49f2a5b14394
Salar Moghaddam <salarmgh@gmail.com>
2024-07-05 16:10:15 +0330

    Init personal config

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,5 +1,54 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'RRethy/nvim-treesitter-endwise',
+    config = function()
+      require('nvim-treesitter.configs').setup {
+        endwise = {
+          enable = true,
+        },
+      }
+    end,
+  },
+  {
+    'windwp/nvim-ts-autotag',
+    config = function()
+      require('nvim-ts-autotag').setup()
+    end,
+  },
+  {
+    'f-person/git-blame.nvim',
+    event = 'BufRead',
+    config = function()
+      vim.cmd 'highlight default link gitblame SpecialComment'
+      require('gitblame').setup { enabled = true }
+    end,
+  },
+  {
+    'nacro90/numb.nvim',
+    event = 'BufRead',
+    config = function()
+      require('numb').setup {
+        show_numbers = true,
+        show_cursorline = true,
+      }
+    end,
+  },
+  {
+    'rmagatti/goto-preview',
+    config = function()
+      require('goto-preview').setup {
+        width = 120, -- Width of the floating window
+        height = 25, -- Height of the floating window
+        default_mappings = false, -- Bind default mappings
+        debug = false, -- Print debug information
+        opacity = nil, -- 0-100 opacity level of the floating window where 100 is fully transparent.
+        post_open_hook = nil, -- A function taking two arguments, a buffer and a window to be ran as a hook.
+        -- You can use "default_mappings = true" setup option
+        -- Or explicitly set keybindings
+      }
+      vim.cmd "nnoremap gpd <cmd>lua require('goto-preview').goto_preview_definition()<CR>"
+      vim.cmd "nnoremap gpi <cmd>lua require('goto-preview').goto_preview_implementation()<CR>"
+      vim.cmd "nnoremap gP <cmd>lua require('goto-preview').close_all_win()<CR>"
+    end,
+  },
+}
